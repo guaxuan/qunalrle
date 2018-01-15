b39b8a6c756ab999f0be9d2439caf61ccab93ec0
hey <1581530482@qq.com>
2018-01-15 16:49:24 +0800

    hey

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/index'
-import City from '@/pages/city'
 
 Vue.use(Router)
 
@@ -11,10 +10,6 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
-    }, {
-      path: '/city',
-      name: 'City',
-      component: City
     }
   ]
 })
