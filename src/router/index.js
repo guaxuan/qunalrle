91890450a156401f1aad6236185b2c2b4cf5946c
gaoxuan <761480075@qq.com>
2018-01-15 16:10:44 +0800

    delete nothing

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Details from '@/pages/details/details'
 import Index from '@/pages/index'
-import City from '@/pages/city'
 
 Vue.use(Router)
 
@@ -16,10 +15,6 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
-    }, {
-      path: '/city',
-      name: 'City',
-      component: City
     }
   ]
 })
